5b1a0eb60194a38dccaa6947f3287db92254b8d9
Ashok-del341 <ashoknaidu.motupalli@gmail.com>
2024-10-21 14:28:08 +0530

    Add files via upload

---
+++ tst_2.py
@@ -0,0 +1,9 @@
+
+length = float(input("Enter the length (i): "))
+width = float(input("Enter the width (w): "))
+area, perimeter = compute_area_and_perimeter(length, width)
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
+#write python to compute spead using s=ut+1/2a+2 using function
+
+#write python program to calculate BMI (body
